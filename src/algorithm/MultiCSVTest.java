5c8ed2478305e104a706c8fd5596640089926b0f
Evgeny Vendrov <evgenyv94@gmail.com>
2018-12-02 12:04:58 +0200

    one more teset for CSV2KML functions

--- src/algorithm/MultiCSVTest.java
+++ src/algorithm/MultiCSVTest.java
@@ -2,7 +2,7 @@ package algorithm;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.File;
+import java.io.*;
 import java.util.Iterator;
 
 import org.junit.jupiter.api.Test;
@@ -14,13 +14,17 @@ import GIS.GisElement;
 import GIS.GisLayer;
 import GIS.GisProject;
 
+//by testing this class we test the KML2CSV class too, as all of these methods use CSV2KML's methods
 class MultiCSVTest {
+	// this is the paths we about to use in the tests
+	private static final String inputPath = "C:" + File.separator + "Users" + File.separator + "evgen" + File.separator
+			+ "eclipse-workspace" + File.separator + "OopAssignment2-4" + File.separator + "csvFilesTest";
+	final String outputPathKML = "C:" + File.separator + "Users" + File.separator + "evgen" + File.separator
+			+ "eclipse-workspace" + File.separator + "OopAssignment2-4" + File.separator + "kmlFilesOutPut";
 
 	@Test
 	void testReadFolder() {
-		// the tester CSV's was put in this folder:
-		String inputPath = "C:" + File.separator + "Users" + File.separator + "evgen" + File.separator
-				+ "eclipse-workspace" + File.separator + "OopAssignment2-4" + File.separator + "csvFilesTest";
+		// creating a project from the tester CSV files
 		GisProject testProject = MultiCSV.readFolder(inputPath);
 		Iterator<GIS_layer> it = testProject.iterator();
 		int counter;
@@ -32,7 +36,7 @@ class MultiCSVTest {
 				fail("not enough csv lines scanned");
 			}
 			// we planted a unique GPS coord in the 122th line of both CSV files, now we're
-			// asserting that this GPS coord was read correctly 
+			// asserting that this GPS coord was read correctly
 			counter = 0;
 			Iterator<GIS_element> itLayer = current.iterator();
 			while (itLayer.hasNext()) {
@@ -50,9 +54,29 @@ class MultiCSVTest {
 		}
 	}
 
-//	@Test
-//	void testFolder2Kml() {
-//		fail("Not yet implemented");
-//	}
+	@Test
+	void testFolder2Kml() {
+		// creating a GIS project as we tested earlier
+		GisProject testProject = MultiCSV.readFolder(inputPath);
+		// converting the project to a file in the output path
+		MultiCSV.folder2Kml(testProject.getArray(),outputPathKML + File.separator + "kmlTESTER.kml");
+		// reading the file, this way we know it is indeed was created
+		BufferedReader br = null;
+		FileReader fr = null;
+
+		try {
+			fr = new FileReader(outputPathKML + File.separator + "kmlTESTER.kml");
+			br = new BufferedReader(fr);
+			String sCurrentLine = "";
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+			}
+		} catch (IOException e) {
+			// if there was an ERROR in reading the file => the file wasn't created
+			// correctly
+			fail("the file wasnt created - folder 2 kml");
+		}
+
+	}
 
 }
